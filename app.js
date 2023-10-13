38e39bb0ac02f32a1affeba73a38ba07d39f8456
manavptl <manavpatel14072002@gmail.com>
2023-10-13 23:51:10 +0530

    Render images upload

--- app.js
+++ app.js
@@ -36,3 +36,43 @@ app.use('/order', order_routes);
 app.listen(process.env.PORT, () => {
     console.log('server is listening on port 3000');
 });
+
+
+
+//render image file
+const fs = require('fs');
+
+const uploadDirectory = path.join(__dirname, 'uploads');
+
+if (!fs.existsSync(uploadDirectory)) {
+    fs.mkdirSync(uploadDirectory);
+}
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log(req.file);
+    // Handle the uploaded image and save its information in your database, if needed.
+    res.json({ message: 'Image uploaded successfully' });
+});
+
+app.get('/upload', (req, res) => {
+    const filename = req.query.filename;
+    if (!filename) {
+        return res.status(400).json({ error: 'Missing filename query parameter' });
+    }
+
+    const filePath = path.join(uploadDirectory, filename);
+    res.sendFile(filePath);
+});  
